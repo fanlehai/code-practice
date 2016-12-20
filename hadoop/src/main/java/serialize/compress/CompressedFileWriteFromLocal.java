f15ee5a60d502f177a000e12beb66700d75cb9dd
fanlehai <fanlehai@163.com>
2016-12-20 22:44:04 +0800

    增加hadoop对map输出使用压缩；对输出文件进行压缩；压缩方式是lzop和snappy等

---
+++ hadoop/src/main/java/serialize/compress/CompressedFileWriteFromLocal.java
@@ -0,0 +1,41 @@
+package serialize.compress;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
+import org.apache.hadoop.io.compress.CompressionCodec;
+import org.apache.hadoop.util.ReflectionUtils;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/*
+ * 读取本地文件，压缩后存储到hdfs上
+ * 
+ * 命令行参数：
+ *  org.apache.hadoop.io.compress.SnappyCodec InputResource/sort/input.txt lcom
+ */
+
+
+public class CompressedFileWriteFromLocal {
+	public static void main(String... args) throws Exception {
+		
+		
+		Configuration config = new Configuration();
+		FileSystem hdfs = FileSystem.get(config);
+
+		Class<?> codecClass = Class.forName(args[0]);
+		CompressionCodec codec = (CompressionCodec) ReflectionUtils.newInstance(codecClass, config);
+
+		InputStream is = FileSystem.getLocal(config).open(new Path(args[1]));
+		OutputStream os = hdfs.create(new Path(args[2] + codec.getDefaultExtension()));
+
+		OutputStream cos = codec.createOutputStream(os);
+
+		IOUtils.copyBytes(is, cos, config, true);
+
+		IOUtils.closeStream(os);
+		IOUtils.closeStream(is);
+	}
+}
---
+++ hadoop/src/main/java/serialize/compress/FileDecompressor.java
@@ -0,0 +1,58 @@
+
+package serialize.compress;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
+import org.apache.hadoop.io.compress.CompressionCodec;
+import org.apache.hadoop.io.compress.CompressionCodecFactory;
+
+/*
+ * 根据文件扩展名选取相应的codec解压文件
+ * 
+ * export HADOOP_CLASSPATH=compress.jar
+ * hadoop serialize.compress.FileDecompressor out.snappy
+ * 
+ */
+public class FileDecompressor {
+
+	public static void main(String[] args) throws Exception {
+		String uri = args[0];
+		Configuration conf = new Configuration();
+
+		Path inputPath = new Path(uri);
+		CompressionCodecFactory factory = new CompressionCodecFactory(conf);
+		CompressionCodec codec = factory.getCodec(inputPath);
+		if (codec == null) {
+			System.err.println("No codec found for " + uri);
+			System.exit(1);
+		}
+		
+		String outputUri = CompressionCodecFactory.removeSuffix(uri, codec.getDefaultExtension());
+
+		FileSystem.get(conf).delete(new Path(outputUri), true);
+		InputStream in = null;
+		FileOutputStream out = null;
+		try {
+			File file = new File(uri);
+			FileInputStream fileInputStream = new FileInputStream(file);
+			in = codec.createInputStream(fileInputStream);
+			File fileOut = new File(outputUri);
+			out = new FileOutputStream(fileOut);
+			IOUtils.copyBytes(in, out, conf);
+		} finally {
+			IOUtils.closeStream(in);
+			IOUtils.closeStream(out);
+		}
+		
+		
+		System.out.println("******* done! *******");
+	}
+}
+// ^^ FileDecompressor
