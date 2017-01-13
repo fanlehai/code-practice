fc95de97c703ae98ef8923470e6e97c97b2ae7fc
fanlehai <fanlehai@163.com>
2017-01-13 14:08:15 +0800

    增加java的string总结

---
+++ java/src/main/java/com/fanlehai/java/string/InfiniteRecursion.java
@@ -0,0 +1,21 @@
+package com.fanlehai.java.string;
+
+// Accidental recursion.
+// {RunByHand}
+import java.util.*;
+
+public class InfiniteRecursion {
+	public String toString() {
+		// 这里的this也会调用他此toStrng方法，导致递归循环栈溢出
+		// 把this换成super.toString即可；
+		return " InfiniteRecursion address: " + this + "\n";
+	}
+
+	public static void main(String[] args) {
+		List<InfiniteRecursion> v = new ArrayList<InfiniteRecursion>();
+		for (int i = 0; i < 10; i++)
+			v.add(new InfiniteRecursion());
+		System.out.println(v);
+		
+	}
+} /// :~
